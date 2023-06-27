1b39c60a3f63f75c178e333b0da3b017ba75c255
Daniel Stefanelli <d1as2n3i4e5l6>
2023-06-27 07:15:50 +0200

    refactor: format

--- tema-4/gke-2/main.tf
+++ tema-4/gke-2/main.tf
@@ -4,8 +4,8 @@ provider "google" {
 }
 
 module "gke_auth" {
-  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-  version = "24.1.0"
+  source       = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  version      = "24.1.0"
   depends_on   = [module.gke]
   project_id   = var.project_id
   location     = module.gke.location
@@ -54,25 +54,25 @@ provider "kubernetes" {
 }
 
 module "gke" {
-  source                 = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
-  version                = "24.1.0"
-  project_id             = var.project_id
-  name                   = "${var.cluster_name}-${var.env_name}"
-  regional               = true
-  region                 = var.region
-  network                = module.gcp-network.network_name
-  subnetwork             = module.gcp-network.subnets_names[0]
-  ip_range_pods          = var.ip_range_pods_name
-  ip_range_services      = var.ip_range_services_name
-  
+  source            = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+  version           = "24.1.0"
+  project_id        = var.project_id
+  name              = "${var.cluster_name}-${var.env_name}"
+  regional          = true
+  region            = var.region
+  network           = module.gcp-network.network_name
+  subnetwork        = module.gcp-network.subnets_names[0]
+  ip_range_pods     = var.ip_range_pods_name
+  ip_range_services = var.ip_range_services_name
+
   node_pools = [
     {
-      name                      = "node-pool"
-      machine_type              = "e2-medium"
-      node_locations            = "europe-west1-b,europe-west1-c,europe-west1-d"
-      min_count                 = 1
-      max_count                 = 2
-      disk_size_gb              = 30
-    },
+      name           = "node-pool"
+      machine_type   = "e2-medium"
+      node_locations = "europe-west1-b,europe-west1-c,europe-west1-d"
+      min_count      = 1
+      max_count      = 2
+      disk_size_gb   = 30
+    }
   ]
 }
