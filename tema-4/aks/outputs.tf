82c3fecbb14f671bbf6a851d63799c0f0bedd098
Daniel Stefanelli <d1as2n3i4e5l6>
2023-06-27 23:47:22 +0200

    feat: format files

--- tema-4/aks/outputs.tf
+++ tema-4/aks/outputs.tf
@@ -1,9 +1,9 @@
 output "resource_group_name" {
   description = "El nombre del grupo de recursos"
-  value = azurerm_resource_group.default.name
+  value       = azurerm_resource_group.default.name
 }
 
 output "kubernetes_cluster_name" {
   description = "El nombre del clúster"
-  value = azurerm_kubernetes_cluster.default.name
+  value       = azurerm_kubernetes_cluster.default.name
 }
