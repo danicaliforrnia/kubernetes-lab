6c0b47046b6b53411d437e0d96baee8226cb1974
Daniel Stefanelli <d1as2n3i4e5l6>
2023-07-20 14:42:06 +0200

    feat: create e2e tests for gke cluster

---
+++ tema-12/leccion-3/cloud_tests/main_test.go
@@ -0,0 +1,50 @@
+package cloud
+
+
+import (
+	"os"
+	"testing"
+
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp" 
+
+	"sigs.k8s.io/e2e-framework/klient/conf"
+	"sigs.k8s.io/e2e-framework/pkg/env"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+	"sigs.k8s.io/e2e-framework/pkg/envfuncs"
+)
+
+var testenv env.Environment
+
+func TestMain(m *testing.M) {
+	testenv = env.New()
+	namespace := envconf.RandomName("sample-ns", 16)
+
+	if os.Getenv("REAL_CLUSTER") == "true" {
+		path := conf.ResolveKubeConfigFile()
+		cfg := envconf.NewWithKubeConfig(path)
+		testenv = env.NewWithConfig(cfg)
+
+		testenv.Setup(
+			envfuncs.CreateNamespace(namespace),
+		)
+		
+		testenv.Finish(
+			envfuncs.DeleteNamespace(namespace),
+		)
+	} else {
+		kindClusterName := envconf.RandomName("kind-with-config", 16)
+
+		testenv.Setup(
+			envfuncs.CreateKindCluster(kindClusterName),
+			envfuncs.CreateNamespace(namespace),
+		)
+
+		testenv.Finish(
+			envfuncs.DeleteNamespace(namespace),
+			envfuncs.DestroyKindCluster(kindClusterName),
+		)
+	}
+	}
+
+	os.Exit(testenv.Run(m))
+}
