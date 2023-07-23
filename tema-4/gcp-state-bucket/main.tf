0236d4fbae5c99a038ea631ae2709287e2deee36
Daniel Stefanelli <d1as2n3i4e5l6>
2023-07-23 13:07:22 +0200

    feat: create terrafom project to provision a GCP bucket

---
+++ tema-4/gcp-state-bucket/main.tf
@@ -0,0 +1,52 @@
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+data "google_storage_project_service_account" "gcs_account" {
+}
+
+resource "google_project_iam_member" "default" {
+  project = var.project_id
+  role    = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  member  = "serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"
+}
+
+resource "google_kms_key_ring" "terraform_state" {
+  name     = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+  location = var.region
+}
+
+resource "google_kms_crypto_key" "terraform_state_bucket" {
+  name            = "test-terraform-state-bucket"
+  key_ring        = google_kms_key_ring.terraform_state.id
+  rotation_period = "86400s"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+  location      = var.region
+  force_destroy = false
+  storage_class = "STANDARD"
+
+  versioning {
+    enabled = true
+  }
+
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.terraform_state_bucket.id
+  }
+
+  depends_on = [
+    google_project_iam_member.default
+  ]
+}
---
+++ tema-4/gcp-state-bucket/variables.tf
@@ -0,0 +1,7 @@
+variable "project_id" {
+  description = "El ID del proyecto donde estará el bucket"
+}
+
+variable "region" {
+  description = "La región donde estará el bucket"
+}
