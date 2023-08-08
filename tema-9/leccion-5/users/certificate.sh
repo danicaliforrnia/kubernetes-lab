55a359476d30c31ad7ae2aa491b7f361854f442f
Daniel Stefanelli <d1as2n3i4e5l6>
2023-08-08 18:07:12 +0200

    feat: update script

--- tema-9/leccion-5/users/certificate.sh
+++ tema-9/leccion-5/users/certificate.sh
@@ -9,10 +9,25 @@ openssl req -new \
 	-out certs/demouser.csr \
 	-subj "/CN=demouser/"
 
-# Crear Certificado
+# Crear Certificado - Opción 1
 openssl x509 \
 	-req -in certs/demouser.csr \
 	-CA ~/.minikube/ca.crt \
 	-CAkey ~/.minikube/ca.key \
 	-CAcreateserial -out certs/demouser.crt \
 	-days 365
+
+# Crear Certificado - Opción 2
+cat <<EOF | kubectl apply -f -
+apiVersion: certificates.k8s.io/v1
+kind: CertificateSigningRequest
+metadata:
+  name: demouser-csr
+spec:
+  request: $(cat certs/demouser.csr | base64 | tr -d '\n')
+  signerName: kubernetes.io/kube-apiserver-client
+  groups:
+    - 'system:authenticated'
+  usages:
+    - client auth
+EOF
