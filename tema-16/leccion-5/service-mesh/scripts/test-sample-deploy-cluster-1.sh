0259fa4ac34d12eb3c06ae3e5a4c08050932b06f
Daniel Stefanelli <d1as2n3i4e5l6>
2023-08-19 10:49:15 +0200

    feat: reorganize into folders

--- tema-16/leccion-5/service-mesh/scripts/test-sample-deploy-cluster-1.sh
+++ tema-16/leccion-5/service-mesh/scripts/test-sample-deploy-cluster-1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CTX_CLUSTER1=cluster-1
+CTX_CLUSTER1=cluster1
 for i in $(seq 10); do kubectl exec --context=$CTX_CLUSTER1 -n sample -c sleep \
     "$(kubectl get pod --context=$CTX_CLUSTER1 -n sample -l \
     app=sleep -o jsonpath='{.items[0].metadata.name}')" \
--- tema-16/leccion-5/service-mesh/scripts/test-sample-deploy-cluster-2.sh
+++ tema-16/leccion-5/service-mesh/scripts/test-sample-deploy-cluster-2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CTX_CLUSTER2=cluster-2
+CTX_CLUSTER2=cluster2
 for i in $(seq 10); do kubectl exec --context=$CTX_CLUSTER2 -n sample -c sleep \
     "$(kubectl get pod --context=$CTX_CLUSTER2 -n sample -l \
     app=sleep -o jsonpath='{.items[0].metadata.name}')" \
