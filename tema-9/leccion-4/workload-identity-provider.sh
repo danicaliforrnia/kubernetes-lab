2f8d458b876450b1fa90545afaaeebf460871a09
Daniel Stefanelli <d1as2n3i4e5l6>
2023-09-03 17:34:40 +0200

    feat: add workload identity federation resources

---
+++ tema-9/leccion-4/workload-identity-provider.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Obtener ID del proyecto
+PROJECT_ID=$(gcloud config get-value project)
+
+echo "Creating GitHub Actions service account..."
+# Crear cuenta de servicio para GitHub
+gcloud iam service-accounts create github-actions \
+    --display-name "GitHub Actions service account"
+echo "------------------------------------"
+
+# Obtener la direccion de correos asociada a la cuenta de servicio
+SERVICE_ACCOUNT_EMAIL=$(gcloud iam service-accounts list \
+  --filter="displayName:GitHub Actions service account" \
+  --format 'value(email)')
+
+echo "Aassign roles to the GitHub service account..."
+# Asignar roles a la cuenta de servicio de GitHub
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+  --member serviceAccount:${SERVICE_ACCOUNT_EMAIL} \
+  --role roles/artifactregistry.admin
+echo "------------------------------------"
+
+echo "Creating a workload identity pool for GitHub..."
+# Pool de Workload Identity para GitHub
+gcloud iam workload-identity-pools create "github-workload-identity-pool" \
+  --project="${PROJECT_ID}" \
+  --location="global" \
+  --display-name="GitHub workload identity pool"
+echo "------------------------------------"
+
+# Obtener el nombre del Workload Identity Pool
+WORKLOAD_IDENTITY_POOL_NAME=$(gcloud iam workload-identity-pools list \
+  --filter="displayName:GitHub workload identity pool" \
+  --location="global" \
+  --format 'value(name)')
+
+echo "Creating a workload identity provider for GitHub..."
+# Workload Identity Provider de GitHub
+gcloud iam workload-identity-pools providers create-oidc "github-workload-identity-provider" \
+  --project="${PROJECT_ID}" \
+  --location="global" \
+  --workload-identity-pool="github-workload-identity-pool" \
+  --display-name="GitHub workload identity provider" \
+  --attribute-mapping="google.subject=assertion.repository,attribute.actor=assertion.actor,attribute.aud=assertion.aud" \
+  --issuer-uri="https://token.actions.githubusercontent.com"
+echo "------------------------------------"
+
+echo "Impersonate workload identity with service account..."
+# Permitir a la Cuenta de Servicio usar el Workload Identity
+gcloud iam service-accounts add-iam-policy-binding ${SERVICE_ACCOUNT_EMAIL} \
+  --project="${PROJECT_ID}" \
+  --role="roles/iam.workloadIdentityUser" \
+  --member="principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_NAME}/*"
